936c15596c1a156016e835f966d0ded83a8f3aa4
OvidiuDPop <ovidiu.dan@keepler.io>
2019-03-20 09:58:03 +0100

    Improove load, added transform and data cleaning

--- load_to_bigquery/load/main.py
+++ load_to_bigquery/load/main.py
@@ -9,18 +9,16 @@ def load_to_bigquery(event, context):
     url = 'gs://{0}/{1}'.format(bucket, file)
     dataset_ref = client.dataset(dataset_id)
     job_config = bigquery.LoadJobConfig()
-    job_config.autodetect = True
     job_config.skip_leading_rows = 1
     job_config.max_bad_records = 1
     # The source format defaults to CSV, so the line below is optional.
     job_config.source_format = bigquery.SourceFormat.CSV
     load_job = client.load_table_from_uri(
         url,
-        dataset_ref.table('playstore_app'),
+        dataset_ref.table('play_store'),
         job_config=job_config)  # API request
     print('Starting job {}'.format(load_job.job_id))
 
     result = load_job.result()  # Waits for table load to complete.
     print(result)
     print('Job finished.')
-    
