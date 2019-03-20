afe61f7dd7d882f64d17fa88109bd9b4afded8c4
Drimmark <axel.bc099@gmail.com>
2019-03-20 23:24:04 +0100

    CSV bad lines filtered and some transformations added

--- batch_processing_dataflow/play_store_flow.py
+++ batch_processing_dataflow/play_store_flow.py
@@ -11,14 +11,37 @@ class ProcessCSV(beam.DoFn):
     def process(self, element, *args, **kwargs):
         import csv
 
-        formated_element = [element]
+        formated_element = [element.encode('utf8')]
         processed_csv = csv.DictReader(formated_element, fieldnames=['App', 'Category', 'Rating', 'Reviews', 'Size',
-                                                                     'Installs', 'Type', 'Price', 'Content Rating',
-                                                                     'Genres', 'Last Updated', 'Current Ver',
-                                                                         'Android Ver'], delimiter=',')
+                                                                     'Installs', 'Type', 'Price', 'Content_Rating',
+                                                                     'Genres', 'Last_Updated', 'Current_Ver',
+                                                                     'Android_Ver'], delimiter=',')
         processed_fields = processed_csv.next()
-        logging.info('Processed line: {}'.format(processed_csv))
-        return processed_fields
+        if processed_fields.get('Category').replace('.','').isdigit():
+            logging.info(processed_fields)
+        else:
+            return [processed_fields]
+
+
+class ParseRecord(beam.DoFn):
+    def process(self, element, *args, **kwargs):
+        from datetime import datetime
+        def string_to_megabyte(raw_string):
+            if raw_string.endswith('K'):
+                multiplier = 1000
+            elif raw_string.endswith('M'):
+                multiplier = 1000 * 1000
+            else:
+                return None
+            return (float(raw_string[:-1]) * multiplier) / 1000000
+        
+        element['Rating'] = float(element['Rating'])# if element['Rating'] is not None else None
+        element['Size'] = string_to_megabyte(element['Size'])
+        element['Price'] = float(element['Price'].replace("$",""))
+        element['Installs'] = int(element['Installs'].replace("+", "").replace(",",""))
+        element['Last_Updated'] = datetime.strptime(element['Last_Updated'], '%B %d, %Y').isoformat()
+        logging.info(element)
+        return [element]
 
 
 def run(argv=None):
@@ -41,19 +64,23 @@ def run(argv=None):
 
     pipeline_options = PipelineOptions(pipeline_args)
     with beam.Pipeline(options=pipeline_options) as pipeline:
-        lines = pipeline | 'ReadFromCsv' >> ReadFromText(known_args.input, skip_header_lines=1)
+        raw_lines = pipeline | 'ReadFromCsv' >> ReadFromText(known_args.input, skip_header_lines=1)
 
-        output = lines | 'processCsv' >> beam.ParDo(ProcessCSV())
+        lines = raw_lines | 'processCsv' >> beam.ParDo(ProcessCSV())
+        
+        output = lines | 'parseRecord' >> beam.ParDo(ParseRecord())
 
         output | WriteToBigQuery(known_args.table_output,
-                                 write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
-                                 create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER)
+                                write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                                create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER)
 
         result = pipeline.run()
         result.wait_until_finish()
         logging.info('Finished.')
+        logging.info('result: %s', result)
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     run()
+
