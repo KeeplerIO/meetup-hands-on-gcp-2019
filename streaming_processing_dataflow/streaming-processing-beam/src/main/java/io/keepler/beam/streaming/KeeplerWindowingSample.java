cd7cee514ffa8a38247727f3e2bfcf96f70a53df
Joaquin Navarro <joaquin.navarro@keepler.io>
2019-03-31 12:55:10 +0200

    Extract common code to independent classes

--- streaming_processing_dataflow/streaming-processing-beam/src/main/java/io/keepler/beam/streaming/KeeplerWindowingSample.java
+++ streaming_processing_dataflow/streaming-processing-beam/src/main/java/io/keepler/beam/streaming/KeeplerWindowingSample.java
@@ -24,11 +24,9 @@
 package io.keepler.beam.streaming;
 
 import com.google.api.services.bigquery.model.TableRow;
-import org.apache.avro.reflect.Nullable;
+import io.keepler.beam.streaming.model.AppRecord;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.coders.AvroCoder;
-import org.apache.beam.sdk.coders.DefaultCoder;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.Description;
@@ -41,17 +39,9 @@ import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * An example that reads from Cloud Storage and writes the average ratings for each category
@@ -89,193 +79,6 @@ public class KeeplerWindowingSample {
     }
 
     /**
-     * Class to hold info about an App.
-     */
-    @DefaultCoder(AvroCoder.class)
-    static class AppRecord {
-
-        String app;
-        @Nullable
-        String category;
-        @Nullable
-        Float rating;
-        @Nullable
-        Integer reviews;
-        @Nullable
-        Float size;
-        @Nullable
-        Integer installs;
-        @Nullable
-        String type;
-        @Nullable
-        Float price;
-        @Nullable
-        String contentRating;
-        @Nullable
-        String genres;
-        @Nullable
-        Date lastUpdated;
-        @Nullable
-        String currentVersion;
-        @Nullable
-        String androidVersion;
-
-        public AppRecord() {}
-
-        public AppRecord(String app, String category, Float rating, Integer reviews, Float size,
-                         Integer installs, String type, Float price, String content_rating,
-                         String genres, Date last_updated, String current_version,
-                         String android_version) {
-            this.app = app;
-            this.category = category;
-            this.rating = rating;
-            this.reviews = reviews;
-            this.size = size;
-            this.installs = installs;
-            this.type = type;
-            this.price = price;
-            this.contentRating = content_rating;
-            this.genres = genres;
-            this.lastUpdated = last_updated;
-            this.currentVersion = current_version;
-            this.androidVersion = android_version;
-        }
-
-        public String getApp() {
-            return app;
-        }
-
-        public String getCategory() {
-            return category;
-        }
-
-        public Float getRating() {
-            return rating;
-        }
-
-        public Integer getReviews() {
-            return reviews;
-        }
-
-        public Float getSize() {
-            return size;
-        }
-
-        public Integer getInstalls() {
-            return installs;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public Float getPrice() {
-            return price;
-        }
-
-        public String getContentRating() {
-            return contentRating;
-        }
-
-        public String getGenres() {
-            return genres;
-        }
-
-        public Date getLastUpdated() {
-            return lastUpdated;
-        }
-
-        public String getCurrentVersion() {
-            return currentVersion;
-        }
-
-        public String getAndroidVersion() {
-            return androidVersion;
-        }
-    }
-
-    static class ParseAppRecordFn extends DoFn<String, AppRecord> {
-
-        private static final Logger LOG = LoggerFactory.getLogger(ParseAppRecordFn.class);
-        //private final Counter numParseErrors = Metrics.counter("main", "ParseErrors");
-
-        private Float parseRating(String rating) {
-            try {
-                float ratingFloat = Float.parseFloat(rating);
-                if (Float.isNaN(ratingFloat)) return null;
-                return ratingFloat;
-            } catch (NumberFormatException e) {
-                return null;
-            }
-        }
-
-        private Float parseSize(String size) {
-            String sizeUpper = size.toUpperCase();
-            int multiplier;
-            switch (sizeUpper.charAt(sizeUpper.length()-1)) {
-                case 'K':
-                    multiplier = 1000;
-                    break;
-                case 'M':
-                    multiplier = 1000 * 1000;
-                    break;
-                default:
-                    multiplier = 1;
-            }
-            String sizeNumber = sizeUpper.replace("M", "").replace("K","");
-            try {
-                return (Float.parseFloat(sizeNumber) * multiplier) / 1000000; // size in MB
-            } catch (NumberFormatException e) {
-                return null;
-            }
-        }
-
-        private Integer parseInstalls(String installs) {
-            try {
-                return Integer.parseInt(installs.replace("+", "").replace(",",""));
-            } catch (NumberFormatException e) {
-                return null;
-            }
-        }
-
-        private Date parseLastUpdated(String lastUpdated) {
-            try {
-                return new SimpleDateFormat("MMMM d, yyyy").parse(lastUpdated);
-            } catch (ParseException e) {
-                return null;
-            }
-        }
-
-        @ProcessElement
-        public void processElement(ProcessContext c) {
-            try {
-                CSVParser csvParser = CSVParser.parse(c.element(), CSVFormat.RFC4180.withHeader(HEADER.split(",")));
-                for (CSVRecord csvRecord : csvParser) {
-                    String app = csvRecord.get("App");
-                    String category = csvRecord.get("Category");
-                    Float rating = parseRating(csvRecord.get("Rating"));
-                    Integer reviews = Integer.parseInt(csvRecord.get("Reviews"));
-                    Float size = parseSize(csvRecord.get("Size"));
-                    Integer installs = parseInstalls(csvRecord.get("Installs"));
-                    String type = csvRecord.get("Type");
-                    Float price = Float.parseFloat(csvRecord.get("Price").replace("$",""));
-                    String contentRating = csvRecord.get("Content Rating");
-                    String genres = csvRecord.get("Genres");
-                    Date lastUpdated = parseLastUpdated(csvRecord.get("Last Updated"));
-                    String currentVersion = csvRecord.get("Current Ver");
-                    String androidVersion = csvRecord.get("Android Ver");
-                    AppRecord appRecord = new AppRecord(app, category, rating, reviews, size, installs, type,
-                            price, contentRating, genres, lastUpdated, currentVersion, androidVersion);
-                    c.output(appRecord);
-                }
-            } catch (Exception e) {
-                // numParseErrors.inc();
-                LOG.info("Parse error on " + c.element() + ", " + e.getMessage());
-            }
-        }
-    }
-
-    /**
      * Converts AppRecords into BigQuery rows.
      */
     static class AppRecordToRowConverter extends DoFn<KV<String, Double>, TableRow> {
@@ -304,7 +107,7 @@ public class KeeplerWindowingSample {
         }
     }
 
-    static void runKeeplerSample(KeeplerSampleOptions options) {
+    static void runKeeplerWindowingSample(KeeplerSampleOptions options) {
         options.setStreaming(true);
         Pipeline p = Pipeline.create(options);
 
@@ -313,7 +116,7 @@ public class KeeplerWindowingSample {
         PCollection<AppRecord> apps = p
                 .apply("ReadLines", PubsubIO.readStrings().fromTopic(options.getTopic()))
                 .apply("Filter CSV Header", ParDo.of(new FilterCSVHeaderFn(HEADER)))
-                .apply("ParseAppRecord", ParDo.of(new ParseAppRecordFn()));
+                .apply("ParseAppRecord", ParDo.of(new ParseAppRecordFn(HEADER)));
 
         PCollection<KV<String, Double>> avgRating = apps
                 .apply("TimeWindow",
@@ -335,6 +138,6 @@ public class KeeplerWindowingSample {
         KeeplerSampleOptions options =
                 PipelineOptionsFactory.fromArgs(args).withValidation().as(KeeplerSampleOptions.class);
 
-        runKeeplerSample(options);
+        runKeeplerWindowingSample(options);
     }
 }
