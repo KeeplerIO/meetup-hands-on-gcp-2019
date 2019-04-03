94fe4c9df484eeecded745bc2393176a48665380
axel.blanco <axel.blanco@keepler.io>
2019-04-03 16:19:28 +0200

    Step processing start

--- load_to_bigquery/extract_transform/main.py
+++ load_to_bigquery/extract_transform/main.py
@@ -13,14 +13,6 @@ def transform_to_mb(val):
 
 
 def extract_and_transform(event, context):
-    bucket = event['bucket']
-    file = event['name']
-    url = 'gs://{0}/{1}'.format(bucket, file)
-    dataset = pd.read_csv(url, sep=',', header=0)
-    dataset.dropna(subset=['Android Ver'], inplace=True)
-    dataset['Installs'] = dataset['Installs'].str.replace("+", "").str.replace(",", "").astype(int)
-    dataset['Last Updated'] = pd.to_datetime(dataset['Last Updated'], format='%B %d, %Y')
-    dataset['Size'] = dataset['Size'].apply(transform_to_mb)
-    bucket_name = os.environ.get('BUCKET', 'meetup-step-processing-output')
-    dataset.to_csv('gs://{0}/{1}'.format(bucket_name, file), sep=',', header=True, index=False)
+    <YOUR CODE>
+    
     print('Job finished.')
